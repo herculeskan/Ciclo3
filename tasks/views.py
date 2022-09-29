2abd7722e64f4493ca066ca1797d6b26a7c98910
herculeskan <carloseara034@gmail.com>
2022-09-29 14:55:32 -0500

    cosas

--- tasks/views.py
+++ tasks/views.py
@@ -1,28 +1,33 @@
-from django.client import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 # Create your views here.
 
-def home(request):
-   return render(request, 'home.html')
 
+def home(request):
+    return render(request, 'home.html')
 
 
 def signup(request):
     if request.method == 'GET':
-        return render(request, 'signup.html', {"form": UserCreationForm})
-    else:
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
 
-        if request.POST["password1"] == request.POST["password2"]:
+    else:
+        if request.POST['password1'] == request.POST['password2']:
             try:
                 user = User.objects.create_user(
-                    request.POST["username"], password=request.POST["password1"])
+                    username=request.POST['username'], password=request.POST['password1'])
                 user.save()
-                login(request, user)
-                return redirect('tasks')
-            except IntegrityError:
-                return render(request, 'signup.html', {"form": UserCreationForm, "error": "Nombre de usuario ya existe."})
-
-        return render(request, 'signup.html', {"form": UserCreationForm, "error": "Las contraseñas no concuerdan."})
+                return HttpResponse('Usuario creado con exito')
+            except:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'El usuario ya existe'
+                })
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            'error':'Las contraseñas no coinciden'
+        })
