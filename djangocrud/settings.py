012720a5a505284aec6007b3cb9501ed2868554d
herculeskan <carloseara034@gmail.com>
2022-10-09 19:03:56 -0500

    CIclo3

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -35,7 +35,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://example.com",
     "https://sub.example.com",
     "http://localhost:8080",
-    "http://127.0.0.1:9000"
+    "http://127.0.0.1:9000",
 ]
 
 CORS_ALLOWED_ORIGINS = True
